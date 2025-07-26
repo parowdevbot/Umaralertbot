fc2f447ade9d48e51fe55f611c80352548344781
parowdevbot <alumbasventures@gmail.com>
2025-07-26 09:01:04 +0100

    Update logger.py

--- utils/logger.py
+++ utils/logger.py
@@ -1,1 +1,12 @@
+import logging
+from config import Config
 
+def setup_logger():
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        handlers=[
+            logging.FileHandler(Config.LOG_FILE),
+            logging.StreamHandler()
+        ]
+    )
