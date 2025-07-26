64a06306c79d0e8501553e1845bc0f6403bc9f53
parowdevbot <alumbasventures@gmail.com>
2025-07-26 14:03:51 +0100

    Create liquidation_scraper.py

---
+++ utils/liquidation_scraper.py
@@ -0,0 +1,6 @@
+class LiquidationScraper:
+    async def get_clusters(self):
+        """Get >$50M liquidation clusters"""
+        url = "https://www.coinglass.com/LiquidationData"
+        # Implementation using BeautifulSoup
+        return clusters
