7267254203c4e3ddd65091c8e31dd4d5fe4fcc08
parowdevbot <alumbasventures@gmail.com>
2025-07-26 13:42:49 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,16 @@
 import os
 
 class Config:
-    BOT_TOKEN = os.getenv("BOT_TOKEN", "YOUR_TOKEN")
+    # Required configurations
+    BOT_TOKEN = os.getenv("BOT_TOKEN", "7822484786:AAET1gInNrtQTrpiuOaRlfJFNmzPyGArcng")
     WEBHOOK_URL = os.getenv("WEBHOOK_URL", "https://umaralertbot.onrender.com")
     PORT = int(os.getenv("PORT", 5000))
     
-    # Alert Thresholds
-    WHALE_THRESHOLD = 5000000
-    LIQUIDATION_THRESHOLD = 50000000
-    USDT_FLOW_THRESHOLD = 40000000
-    NASDAQ_THRESHOLD = 5.0
+    # Logging configuration
+    LOG_FILE = os.getenv("LOG_FILE", "logs/bot.log")  # Add this line
+    
+    # Alert thresholds
+    WHALE_THRESHOLD = int(os.getenv("WHALE_THRESHOLD", 5000000))
+    LIQUIDATION_THRESHOLD = int(os.getenv("LIQUIDATION_THRESHOLD", 50000000))
+    USDT_FLOW_THRESHOLD = int(os.getenv("USDT_FLOW_THRESHOLD", 40000000))
+    NASDAQ_THRESHOLD = float(os.getenv("NASDAQ_THRESHOLD", 5.0))
