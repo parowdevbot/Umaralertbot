db64f6c82e543e661d9f6e6f928bed1ac3afac35
parowdevbot <alumbasventures@gmail.com>
2025-07-26 16:03:55 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config:
-    BOT_TOKEN = os.getenv("BOT_TOKEN", "7822484786:AAET1gInNrtQTrpiuOaRlfJFNmzPyGArcng")
+    BOT_TOKEN = os.getenv("BOT_TOKEN", "8222743591:AAGbNsISPbPacxhLVQmuAUWuLgTMfPFf9o4")
     WEBHOOK_URL = os.getenv("WEBHOOK_URL", "https://umaralertbot.onrender.com")  # MUST match your Render URL exactly
     PORT = int(os.getenv("PORT", 5000))
