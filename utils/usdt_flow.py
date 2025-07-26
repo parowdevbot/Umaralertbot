1949341ef0720e0f94654c049f677bf56db2fe7b
parowdevbot <alumbasventures@gmail.com>
2025-07-26 14:05:43 +0100

    Create usdt_flow.py

---
+++ utils/usdt_flow.py
@@ -0,0 +1,6 @@
+class USDTFlowTracker:
+    async def get_net_flow(self):
+        """TRC20 USDT flow using Tronscan"""
+        url = "https://tronscan.org/#/token/TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t"
+        # Scraping implementation
+        return net_flow
